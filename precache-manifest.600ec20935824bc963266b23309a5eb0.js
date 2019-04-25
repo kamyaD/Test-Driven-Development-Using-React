38dc71d2dd827d9aae06cd68ab82993649d23ec7
Kamya Domnick <kamya.domnick@andela.com>
2019-04-25 10:22:41 +0300

    Updates

---
+++ precache-manifest.600ec20935824bc963266b23309a5eb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "477cf978f72320f15bec",
+    "url": "/Test-Driven-Development-Using-React/static/js/runtime~main.86c2b85d.js"
+  },
+  {
+    "revision": "31db07ecb45af3a507a6",
+    "url": "/Test-Driven-Development-Using-React/static/js/main.b3f42fd3.chunk.js"
+  },
+  {
+    "revision": "a6510ea29a270a926de5",
+    "url": "/Test-Driven-Development-Using-React/static/js/2.f85f586c.chunk.js"
+  },
+  {
+    "revision": "31db07ecb45af3a507a6",
+    "url": "/Test-Driven-Development-Using-React/static/css/main.e2708267.chunk.css"
+  },
+  {
+    "revision": "16da51f83d328514e915ffd0f65b2310",
+    "url": "/Test-Driven-Development-Using-React/index.html"
+  }
+];
